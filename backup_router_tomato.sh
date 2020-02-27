5271b67375a629cdb39d21bb7a8021cca564ca46
Ben Batschelet <bartleby84@gmail.com>
2020-02-27 12:20:54 -0600

    Flatten dependency set

--- backup_router_tomato.sh
+++ backup_router_tomato.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
+
 ScriptInfo_backup_router() {
-SCRIPT_NAME="Tomato Router Backup Script"; SCRIPT_VERSION="2.7"; SCRIPT_DATE="2014/02/09"; SCRIPT_AUTHER="Austin Saint Aubin"; SCRIPT_AUTHER_CONTACT="AustinSaintAubin@gmail.com"
-SCRIPT_DESCRIPTION="Selctively backup everything on your tomato router"
+SCRIPT_NAME="Tomato Router Backup Script"; SCRIPT_VERSION="3.0"; SCRIPT_DATE="2020-02-27"; SCRIPT_AUTHER="Ben Batschelet"; SCRIPT_AUTHER_CONTACT="ben.batschelet@gmail.com"
+SCRIPT_DESCRIPTION="Selctively backup everything on your Tomato router"
 SCRIPT_TITLE="$SCRIPT_NAME - v$SCRIPT_VERSION - $SCRIPT_DATE - $SCRIPT_AUTHER ($SCRIPT_AUTHER_CONTACT) \n   ∟ Description: $SCRIPT_DESCRIPTION"
 echo -e " $(YEL "▶︎") $SCRIPT_TITLE"; }
 # -------------------------------------------------------------------------------------------------
@@ -14,30 +16,25 @@ SCRIPT_FILENAME="$(basename $0)"
 SCRIPT_DIRECTORY="$(dirname $0)"
 SCRIPT_DEPENDENCIES_DIRECTORY="$SCRIPT_DIRECTORY/dependencies"  # This Scripts Specific Dependencies
 #   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
-SCRIPTS_DIRECTORY="$(dirname "$SCRIPT_DIRECTORY")"  # Scripts Root Directory
-SCRIPTS_DEPENDENCIES_DIRECTORY="$SCRIPTS_DIRECTORY/dependencies"  # All Scripts Gerneral Dependencies
-#   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
 OS_VERSION=$(nvram get os_version)
 OS_VERSION_UNDERSCORED=$(nvram get os_version | sed -e 's/ /_/g')
 
 # [# Passed Variables Defaults #]
 main_backup_destination_directory_name_prefix="tomato_router_backup"
 main_backup_destination_directory_name_suffix=""
-main_backup_destination_directory_root="$(dirname "$(dirname "$SCRIPTS_DIRECTORY")")/backups"
+main_backup_destination_directory_root="/opt/backups"
 main_backup_retention_number=120  # Number of backups to keep
 main_backup_note="$OS_VERSION_UNDERSCORED"
-main_backup_type="2"  # Backup Type ( limited(1)|default(2)|full(3) )
 
-echo "[# Loading Dependencies & Source Scripts #]" # --------------------------------------------------------------------------------------
-source "$SCRIPTS_DEPENDENCIES_DIRECTORY/color_text_functions.sh"  # Include Color Functions
-#source "$SCRIPTS_DEPENDENCIES_DIRECTORY/color_text_functions.sh" disabled  # Include Color Functions (Disabled)
+echo "[# Loading Dependencies & Source Scripts #]"
+source "$SCRIPT_DEPENDENCIES_DIRECTORY/color_text_functions.sh"
 #   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
-source "$SCRIPT_DEPENDENCIES_DIRECTORY/backup_file_functions.sh" "$SCRIPTS_DEPENDENCIES_DIRECTORY" "$SCRIPT_DEPENDENCIES_DIRECTORY"  # Include Backup Files Dependency
-source "$SCRIPT_DEPENDENCIES_DIRECTORY/backup_folder_functions.sh" "$SCRIPTS_DEPENDENCIES_DIRECTORY" "$SCRIPT_DEPENDENCIES_DIRECTORY"  # Include Backup Folders Dependency
-source "$SCRIPT_DEPENDENCIES_DIRECTORY/backup_folder_archive_functions.sh" "$SCRIPTS_DEPENDENCIES_DIRECTORY" "$SCRIPT_DEPENDENCIES_DIRECTORY"  # Include Backup Folders Dependency
-source "$SCRIPT_DEPENDENCIES_DIRECTORY/backup_nvram_encapsulated_functions.sh" "$SCRIPTS_DEPENDENCIES_DIRECTORY" "$SCRIPT_DEPENDENCIES_DIRECTORY"  # Include Backup NVRam Encapsulated Dependency
-source "$SCRIPT_DEPENDENCIES_DIRECTORY/backup_nvram_raw_functions.sh" "$SCRIPTS_DEPENDENCIES_DIRECTORY" "$SCRIPT_DEPENDENCIES_DIRECTORY"  # Include Backup NVRam RAW Dependency
-source "$SCRIPT_DEPENDENCIES_DIRECTORY/backup_sysinfo_functions.sh" "$SCRIPTS_DEPENDENCIES_DIRECTORY" "$SCRIPT_DEPENDENCIES_DIRECTORY"  # Include Backup NVRam RAW Dependency
+source "$SCRIPT_DEPENDENCIES_DIRECTORY/backup_file_functions.sh"
+source "$SCRIPT_DEPENDENCIES_DIRECTORY/backup_folder_functions.sh"
+source "$SCRIPT_DEPENDENCIES_DIRECTORY/backup_folder_archive_functions.sh"
+source "$SCRIPT_DEPENDENCIES_DIRECTORY/backup_nvram_encapsulated_functions.sh"
+source "$SCRIPT_DEPENDENCIES_DIRECTORY/backup_nvram_raw_functions.sh"
+source "$SCRIPT_DEPENDENCIES_DIRECTORY/backup_sysinfo_functions.sh"
 
 # [# Functions #] --------------------------------------------------------------------------------------
 
@@ -48,7 +45,7 @@ ScriptInfo_backup_router
 # [# Read Passed Varables #] --------------------------------------------------------------------------------------
 # Set Scripts Dependencies Directory (Glodal)
 echo -ne " ∟ Backup Type (default|limited|full)"
-if [ -z "$1" ]; then 
+if [ -z "$1" ]; then
 	echo "  [ NOT Passed ]"
 else
 	echo "  [ Passed ]"
@@ -99,7 +96,7 @@ backupNVRamRAW "NVRam RAW Configuration" "$main_backup_destination_directory_pat
 if [ $main_backup_type -ge 2 ]; then
 	backupNVRamEcapsulated "NVRam (Encapsulated)" "$main_backup_destination_directory_path/NVRam" "tomato_nvram_encapsulated_" "$main_backup_note" "_sufix.cfg" "$main_backup_retention_number" "" ""
 	backupNVRamEcapsulated "NVRam (Encapsulated) Filtered" "$main_backup_destination_directory_path/NVRam" "tomato_nvram_encapsulated_filtered_" "$main_backup_note" "_sufix.cfg" "$main_backup_retention_number" "NC|clkfreq|ddnsx|dhcpd|ftp|http|lan_hostname|lan_ipaddr|lan_netmask|lan_proto|log|ntp|qos|rstats|sch|script|sesx|smbd|snmp|sshd|telnetd|tm|usb|vpn_server_|vpn_server1|wan_dns|wan_hostname|web|wl0_" ""
-	
+
 	backupFile "System Log" "/tmp/var/log/messages" "$main_backup_destination_directory_path" "syslog_" "$main_backup_note" ".log" "$main_backup_retention_number"
 	backupFile "Web Usage Domains" "/proc/webmon_recent_domains" "$main_backup_destination_directory_path" "webmon_recent_domains_" "$main_backup_note" ".txt" "$main_backup_retention_number"
 	backupFile "Web Usage Searches" "/proc/webmon_recent_searches" "$main_backup_destination_directory_path" "webmon_recent_searches_" "$main_backup_note" ".txt" "$main_backup_retention_number"
